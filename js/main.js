305e507c58b5c3117b58cf4cf0570f21bba5dbb1
tristan <tristan.wermelinger@gmail.com>
2023-04-21 15:37:18 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded");
+
+  document
+    .querySelector("#formContact")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      console.log("submit", event);
+
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value,
+      };
+      const response = await axios.post("http://localhost:3002/form", {
+        firstname,
+        lastname,
+        email,
+        subject,
+        message,
+      });
+
+      console.log(response.data);
+    });
+});
